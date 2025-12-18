2ac241a7aca44495e653f0ea329500ca87498774
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-12-18 21:41:09 +0700

    menambahkan destinasi Detail pada navigasi

---
+++ app/src/main/java/com/example/questapi_147/uicontroller/route/DestinasiDetail.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_147.uicontroller.route
+
+import com.example.questapi_147.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "item_detail"
+    override val titleRes = R.string.detail_siswa
+}
