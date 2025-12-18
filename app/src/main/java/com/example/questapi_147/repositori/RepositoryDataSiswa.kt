aa933cc01d50d832333c504e9cbc87f0a19b6b65
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-12-18 20:29:54 +0700

    menambahkan repository data siswa berbasis API

---
+++ app/src/main/java/com/example/questapi_147/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,17 @@
+package com.example.questapi_147.repositori
+
+import com.example.questapi_147.apiservice.ServiceApiSiswa
+import com.example.questapi_147.modeldata.DataSiswa
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private  val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun  getDataSiswa() : List<DataSiswa> = serviceApiSiswa
+        .getSiswa()
+    override suspend fun  postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
