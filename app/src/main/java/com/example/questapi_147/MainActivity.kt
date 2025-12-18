271306d015f30e12fbff163110d74187c218dc5d
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-12-18 21:35:23 +0700

    memperbaiki main activity

--- app/src/main/java/com/example/questapi_147/MainActivity.kt
+++ app/src/main/java/com/example/questapi_147/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questapi_147.ui.theme.QuestAPI_147Theme
+import com.example.questapi_147.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAPI_147Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestAPI_147Theme {
-        Greeting("Android")
     }
 }
