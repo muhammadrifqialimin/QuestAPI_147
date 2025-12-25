7f28339c9505c54ff4a217c34d50cbb398018aa6
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-12-26 02:11:26 +0700

    Menetapkan route item_edit dan judul halaman edit_siswa

--- app/src/main/java/com/example/questapi_147/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_147/uicontroller/route/DestinasiEdit.kt
@@ -2,3 +2,5 @@ package com.example.questapi_147.uicontroller.route
 
 
 object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
