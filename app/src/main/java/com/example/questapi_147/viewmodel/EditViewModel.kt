baa8c0d29630e002657f1ca981f0cc1c4fe415d8
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-12-26 02:22:12 +0700

    Menambahkan EditViewModel untuk memuat dan mengelola state edit data siswa

--- app/src/main/java/com/example/questapi_147/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_147/viewmodel/EditViewModel.kt
@@ -1,1 +1,14 @@
 package com.example.questapi_147.viewmodel
+
+class EditViewModel( savedStateHandle: SavedStateHandle,private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UiStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa).toUiStateSiswa(true)
+        }
+    }
