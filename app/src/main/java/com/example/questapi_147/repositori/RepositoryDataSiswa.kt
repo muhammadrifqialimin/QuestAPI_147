28cc3d59070c2e888b521b2e172c675a6e14c7a9
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-12-26 02:27:03 +0700

    Menambahkan fungsi repository untuk ambil, edit, dan hapus data siswa berdasarkan ID

--- app/src/main/java/com/example/questapi_147/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_147/repositori/RepositoryDataSiswa.kt
@@ -3,9 +3,13 @@ package com.example.questapi_147.repositori
 import com.example.questapi_147.apiservice.ServiceApiSiswa
 import com.example.questapi_147.modeldata.DataSiswa
 
-interface RepositoryDataSiswa {
+interface RepositoryDataSiswa{
     suspend fun getDataSiswa() : List<DataSiswa>
-    suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun postDataSiswa(dataSiswa:DataSiswa) :retrofit2.Response<Void>
+
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa(id:Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
