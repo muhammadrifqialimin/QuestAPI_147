ea74595f94dc67d2420fc4b3df32086699efdcd2
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-12-26 02:08:16 +0700

    Menambahkan route detail_siswa untuk navigasi ke halaman detail siswa

--- app/src/main/java/com/example/questapi_147/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_147/uicontroller/route/DestinasiDetail.kt
@@ -3,6 +3,6 @@ package com.example.questapi_147.uicontroller.route
 import com.example.questapi_147.R
 
 object DestinasiDetail : DestinasiNavigasi {
-    override val route = "item_detail"
+    override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
 }
