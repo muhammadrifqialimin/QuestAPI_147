f66af96058c0f119cf97951a7a7d4e946622a8ff
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-12-26 02:14:56 +0700

    Menambahkan StatusUiDetail untuk mengelola state UI detail siswa (Loading, Success, Error)

--- app/src/main/java/com/example/questapi_147/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_147/viewmodel/DetailViewModel.kt
@@ -1,1 +1,7 @@
 package com.example.questapi_147.viewmodel
+
+sealed interface StatusUiDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUiDetail
+    object Error : StatusUiDetail
+    object Loading : StatusUiDetail
+}
