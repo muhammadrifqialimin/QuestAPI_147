a715eea6f98a8d2c72f929dfa70607fd349e6346
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-12-26 02:11:49 +0700

    Menambahkan konstanta idSiswa sebagai argumen navigasi

--- app/src/main/java/com/example/questapi_147/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_147/uicontroller/route/DestinasiEdit.kt
@@ -4,3 +4,5 @@ package com.example.questapi_147.uicontroller.route
 object DestinasiEdit : DestinasiNavigasi {
     override val route = "item_edit"
     override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+}
